0aaf3b8cf9dcac83523b4dc05b27de627fe4c0bb
DravenWB <DravenWB@users.noreply.github.com>
2025-11-22 16:23:22 -0700

    Added ThrowOnFail argument to PS version check. Additional cleanup.

--- src/ocms/Private/PS.Module.Tests.ps1
+++ src/ocms/Private/PS.Module.Tests.ps1
@@ -1,24 +1,24 @@
+
 function Test-OcmsPSVersion {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [version]$Version
+        [version]$Version,
+
+        [ValidateSet($true, $false, IgnoreCase = $true)]
+        [boolean]$ThrowOnFail
     )
+
+if($ThrowOnFail -eq $null) {$ThrowOnFail = $false}
 
 $InstalledVersion = ($PSVersionTable.PSVersion)
 
 Write-Verbose "Now checking PS version."
 
-if ($InstalledVersion -ge '$Version') { 
-
-    Write-Verbose "Powershell version is $InstalledVersion and is greater than required version: $Version"
-    return 
-}
-
+if ($InstalledVersion -ge '$Version') { Write-Verbose "Version test pass. Required: $Version. Current: $InstalledVersion"; return}
     else {
-        Write-Error "The currently running PowerShell version is $InstalledVersion."
-        Write-Error "This PowerShell script requires PowerShell version $Version or greater."
-        throw "Please run in PowerShell $Version and try again."
+        if ($ThrowOnFail == $true) {throw "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion."}
+            else {Write-Error "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion. Unexpected behavior may occur."; continue}
     }
 }
 
@@ -28,10 +28,6 @@ function Test-OcmsPnPInstall {
 
     Write-Verbose "Now checking if PnP.PowerShell is installed."
 
-    if (Get-Module -ListAvailable -Name 'PnP.PowerShell' -ErrorAction SilentlyContinue) {
-        Write-Verbose "Required PnP.PowerShell module is currently installed."
-        Return
-    }
-
-    else { throw "PnP.PowerShell is not installed. Please install the module to continue." }
+    if (Get-Module -ListAvailable -Name 'PnP.PowerShell' -ErrorAction SilentlyContinue) {Write-Verbose "Required PnP.PowerShell module is currently installed."; Return}
+        else {throw "PnP.PowerShell is not installed. Please install the module to continue."}
 }
