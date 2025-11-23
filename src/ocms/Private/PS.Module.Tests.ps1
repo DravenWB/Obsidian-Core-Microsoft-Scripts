570d277bfc1561fa4af5721cc58bd2d3d65b2531
DravenWB <DravenWB@users.noreply.github.com>
2025-11-22 17:06:44 -0700

    Added module auto-installation functionality. Cleaned up code structure. Added additional input validation.

--- src/ocms/Private/PS.Module.Tests.ps1
+++ src/ocms/Private/PS.Module.Tests.ps1
@@ -2,32 +2,79 @@
 function Test-OcmsPSVersion {
     [CmdletBinding()]
     param (
+        [ValidateCount(1)]
         [Parameter(Mandatory)]
         [version]$Version,
 
-        [ValidateSet($true, $false, IgnoreCase = $true)]
+        [ValidateCount(1)]
+        [ValidateSet($true, $false)]
+        [Parameter()]
         [boolean]$ThrowOnFail
     )
 
-if($ThrowOnFail -eq $null) {$ThrowOnFail = $false}
+if($ThrowOnFail -eq $null) {
+    $ThrowOnFail = $true
+}
 
 $InstalledVersion = ($PSVersionTable.PSVersion)
 
 Write-Verbose "Now checking PS version."
 
-if ($InstalledVersion -ge '$Version') { Write-Verbose "Version test pass. Required: $Version. Current: $InstalledVersion"; return}
+if ($InstalledVersion -ge '$Version') { 
+    Write-Verbose "Version test pass. Required: $Version. Current: $InstalledVersion"
+    return
+}
     else {
-        if ($ThrowOnFail == $true) {throw "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion."}
-            else {Write-Error "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion. Unexpected behavior may occur."; continue}
+        if ($ThrowOnFail == $true) {
+            throw "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion."
+        }
+            else {
+                Write-Error "Powershell function requires PowerShell v$Version. Running: v$InstalledVersion. Unexpected behavior may occur."
+                continue
+            }
     }
 }
 
-function Test-OcmsPnPInstall {
+function Test-OcmsModuleInstallation {
     [CmdletBinding()]
-    param ()
+    param (
+        [ValidateCount(1)]
+        [ValidateSet("Sharepoint", "Graph", "PnP", IgnoreCase = $false)]
+        [Parameter(Mandatory)]
+        [string]$Module,
 
-    Write-Verbose "Now checking if PnP.PowerShell is installed."
+        [ValidateCount(1)]
+        [ValidateSet($true, $false)]
+        [Parameter()]
+        [Boolean]$AutoInstall
+    )
 
-    if (Get-Module -ListAvailable -Name 'PnP.PowerShell' -ErrorAction SilentlyContinue) {Write-Verbose "Required PnP.PowerShell module is currently installed."; Return}
-        else {throw "PnP.PowerShell is not installed. Please install the module to continue."}
+    if ($AutoInstall -eq $null) {
+        $AutoInstall = $false
+    }
+
+Write-Verbose "Now checking if $Module is installed."
+
+    try {
+        Get-Module -ListAvailable -Name $Module
+        Write-Verbose "Required $Module module is currently installed."
+        continue
+    }
+        catch {
+            if ($AutoInstall -eq $true) {
+                Write-Verbose "Auto-install enabled. Now installing missing module $Module"
+
+                try {
+                    Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
+                    continue
+                }
+                    catch {
+                        throw "$Module failed to install with error: $_"
+                    }
+
+            else {
+                throw "Required Module: $Module is not currently installed."
+            }
+            }
+        }
 }
