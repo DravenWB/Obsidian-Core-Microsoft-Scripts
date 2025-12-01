e7a1b16a8e0d06bc4f8639bd10f75a9a02eff7ab
DravenWB <46582061+DravenWB@users.noreply.github.com>
2025-12-01 13:41:41 -0700

    Initial version of unified logging function.

---
+++ src/ocms/Private/Write-OcmsLog.ps1
@@ -0,0 +1,54 @@
+Write-OcmsLog {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [object]$Data,
+
+        [ValidateCount(1)]
+        [string]$FileName = "ocms-log",
+
+        [ValidateCount(1)]
+        [string]$LogPath = [Environment]::GetFolderPath("Desktop"),
+
+        [ValidateCount(1)]
+        [bool]$ThrowOnFail = $false
+    )
+}
+
+$CompletePath = $LogPath + "\$FileName"
+
+$LogModifier = Get-Date -Format "MM_dd_yyyy-HHmm"
+
+Write-Verbose "Testing output to $CompletePath"
+
+if (Test-Path $CompletePath) {
+    try {
+        Export-Csv -InputObject $Data -Path $CompletePath
+        Write-Verbose "File at path already exists. Attempting modified file name."
+    
+        $CompletePath = $LogPath + "\$FileName" + "-$LogModifier"
+
+        Write-Verbose "File save successful with modified file name."
+    }
+        catch {
+            Write-Error "Unhandled exception: $($_.Exception.Message)"
+            Write-Verbose "Attempting secondary alternative file name to output file."
+
+            try {
+                $BackupModifier = (Get-Date).Second
+                $CompletePath = $LogPath + "\$FileName" + "-$LogModifier" + "-$BackupModifier"
+                Export-Csv -InputObject $Data -Path $CompletePath
+
+                Write-Verbose "Secondary file succeeded in save operation."
+            }
+
+            catch {
+                if ($ThrowOnFail) {
+                    throw "Unhandled exception: $($_.Exception.Message)"
+                }
+                else {Write-Error "Unhandled exception: $($_.Exception.Message)"} 
+            }
+        }
+}
+
+else {Export-Csv -InputObject $Data -Path $CompletePath}
