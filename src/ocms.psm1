dae6218ad9e1dc94348eec273452ede0628580f5
DravenWB <46582061+DravenWB@users.noreply.github.com>
2025-12-04 16:51:59 -0700

    Update with current modules.

--- src/ocms.psm1
+++ src/ocms.psm1
@@ -2,22 +2,51 @@
 
 # Dot-source private functions
 . "$PSScriptRoot/Private/Connection.Tests.ps1"
-. "$PSScriptRoot/Private/PS.Version.Tests.ps1"
+. "$PSScriptRoot/Private/PS.Module.Tests.ps1"
+. "$PSSCriptRoot/Private/Write-OcmsLog.ps1"
 
 # Dot-source public functions
-. "$PSScriptRoot/Public/SharePoint.Connections.ps1"
-. "$PSScriptRoot/Public/Core.Settings.ps1"
-. "$PSScriptRoot/Public/Identity.Licenses.ps1"
-. "$PSScriptRoot/Public/Purview.Labels.ps1"
+
+  # Global
+    . "$PSScriptRoot/Public/Global/Connect-OcmsService.ps1"
+    . "$PSScriptRoot/Public/Global/Test-OcmsConnection.ps1"
+    . "$PSScriptRoot/Public/Global/Test-OcmsModule.ps1"
+    . "$PSScriptRoot/Public/Global/Write-OcmsLog.ps1"
+
+  # Entra
+    . "$PSScriptRoot/Public/Entra/Invoke-OcmsLicenseMigration.ps1"
+
+  # Purview
+    . "$PSScriptRoot/Public/Purview/Get-OcmsSensitivityLabelPolicy.ps1"
+
+  # SharePoint
+    . "$PSScriptRoot/Public/SharePoint/Get-OcmsSiteOwnerReport.ps1"
+    . "$PSScriptRoot/Public/SharePoint/Invoke-OcmsPermissionsCleanup.ps1"
+    . "$PSScriptRoot/Public/SharePoint/New-OcmsTestListItems.ps1"
+    . "$PSScriptRoot/Public/SharePoint/Rename-OcmsSpoPageUrl.ps1"
+    . "$PSScriptRoot/Public/SharePoint/Repair-OcmsGrouplessTeamSite.ps1"
+    . "$PSScriptRoot/Public/SharePoint/Repair-OcmsUserPuid.ps1"
+    . "$PSScriptRoot/Public/SharePoint/Reset-OcmsLibraryInheritance.ps1"
 
 Export-ModuleMember -Function `
-    'Connect-OcmsSPO',
-    'Connect-OcmsPnPOnline',
-    'Initialize-OcmsProfile',
-    'Get-OcmsSetting',
-    'Invoke-OcmsLicenseMigration',
-    'Get-OcmsSensitivityLabelPolicy',
-    'Export-OcmsSensitivityLabelPolicyReport',
-    'Reset-OcmsSpoLibraryInheritance',
-    'Get-OcmsSiteOwnerReport'
 
+    # Global
+    'Connect-OcmsService',
+    'Test-OcmsConnection',
+    'Test-OcmsModule',
+    'Write-OcmsLog',
+
+    #Entra
+    'Invoke-OcmsLicenseMigration',
+
+    #Purview
+    'Get-OcmsSensitivityLabelPolicy',
+
+    #SharePoint
+    'Get-OcmsSiteOwnerReport',
+    'Invoke-OcmsPermissionsCleanup',
+    'New-OcmsTestListItems',
+    'Rename-OcmsSpoPageUrl',
+    'Repair-OcmsGrouplessTeamSite',
+    'Repair-OcmsUserPuid',
+    'Reset-OcmsLibraryInheritance'
